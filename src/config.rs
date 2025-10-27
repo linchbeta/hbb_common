48de1536992501ede57b54d49e3ee435fda74a0c
linchbeta <linch.beta@gmail.com>
2025-10-27 19:59:37 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -101,8 +101,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["lasx.me"];
+pub const RS_PUB_KEY: &str = "etwpLLaIubP46sRX0cp8hKVM4i47u2m6lmUKTaRXNwo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
